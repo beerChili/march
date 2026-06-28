30bc5b066a3dc9c2d5abb87cbffeb00cdd561fbb
euli <eugen.liebreich@gmail.com>
2026-06-28 13:26:11 +0200

    add firefox setup

---
+++ firefox/install.sh
@@ -0,0 +1,3 @@
+sudo install -Dm644 autoconfig.js /usr/lib/firefox/defaults/pref/autoconfig.js
+sudo install -Dm644 firefox.cfg /usr/lib/firefox/firefox.cfg
+sudo install -Dm644 policies.json /usr/lib/firefox/distribution/policies.json
